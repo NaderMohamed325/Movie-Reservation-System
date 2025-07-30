790d51c6093e8a93d4aa0949251f2ac0cff85043
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-30 12:24:39 +0300

    Add app.ts to set up Express middleware for security and request handling

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import { xss } from "express-xss-sanitizer";
+import { limiter } from "./configs";
+
+const app = express();
+
+app.use(helmet());
+app.use(express.json({ limit: "1kb" }));
+app.use(express.urlencoded({ extended: true, limit: "1kb" }));
+app.use(cors());
+app.use(cookieParser());
+app.use(limiter);
+app.use(xss());
+
+export { app };
