7fa5765a8586eb7566c93a0c8034636199ab882b
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-30 13:20:44 +0300

    Add user router and error handling middleware to index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
+import { NextFunction, Request, Response } from "express";
 import { app } from "./app";
 import { errorLogger, infoLogger } from "./configs";
+import { userRouter } from "./router/userRouter";
 
 //info logger
 app.use(infoLogger);
 
 //routers
+app.use("/api/users", userRouter);
 
 //error logger
 app.use(errorLogger);
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error("Unhandled Error:", err.stack);
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 app.listen(3000, () => {
   console.log(`Server is running on port 3000 on url : http://localhost:3000 `);
 });
