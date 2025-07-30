6e2064538713e289cab9242fc0d2070d92ecf712
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-30 15:58:15 +0300

    Increase JSON and URL-encoded request size limits to 10kb for improved data handling

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ import { limiter } from "./configs";
 const app = express();
 
 app.use(helmet());
-app.use(express.json({ limit: "1kb" }));
-app.use(express.urlencoded({ extended: true, limit: "1kb" }));
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cors());
 app.use(cookieParser());
 app.use(limiter);
