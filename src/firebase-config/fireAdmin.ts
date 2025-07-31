18c8049ef8342bbc045518b91a40ceab370456a8
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-31 20:54:59 +0300

    Update .gitignore to include Firebase configuration JSON files and fix service account import path

--- src/firebase-config/fireAdmin.ts
+++ src/firebase-config/fireAdmin.ts
@@ -1,6 +1,6 @@
 import admin, { ServiceAccount } from "firebase-admin";
 
-import serviceAccount from "./service-account/firebase-service-account.json.json"; // adjust path
+import serviceAccount from "./firebase-service-account.json.json"; // adjust path
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as ServiceAccount),
