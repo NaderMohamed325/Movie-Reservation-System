c69b5b25a99b025bfffc2900260d05f50d104ade
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-31 22:33:37 +0300

    Refactor JWT verification to use imported JWT_SECRET_KEY from envVariable

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import { JWT_SECRET_KEY } from "../envVariable";
 
 export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.token;
@@ -9,7 +10,7 @@ export const authenticateJWT = (req: Request, res: Response, next: NextFunction)
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as { userId: string };
+    const decoded = jwt.verify(token,JWT_SECRET_KEY ) as { userId: string };
 
 
     req.userId = { id: decoded.userId } as any;
