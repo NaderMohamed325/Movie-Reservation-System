141e311f7a8c5a817bd18a842e37b0893c51c13f
NaderMohamed325 <nadoorclasher@gmail.com>
2025-07-31 22:33:43 +0300

    Refactor userRouter to include additional routes for user management and JWT authentication

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,7 +1,22 @@
 import { Router } from "express";
-import { createUser, loginUser } from "../controller/userController";
+import {
+  createUser,
+  getMe,
+  loginUser,
+  logoutUser,
+  patchMe,
+  patchMyPassword,
+} from "../controller/userController";
+import { authenticateJWT } from "../middlewares/isAuth";
 
 const userRouter = Router();
 
-userRouter.post("/register", createUser).post("/login", loginUser);
+userRouter
+  .post("/register", createUser)
+  .post("/login", loginUser)
+  .get("/logout", authenticateJWT, logoutUser)
+  .get("/me", authenticateJWT, getMe)
+  .patch("/me", authenticateJWT, patchMe)
+  .patch("/me/password", authenticateJWT, patchMyPassword);
+
 export { userRouter };
